2240caa05f86d5f4870e45ca4371d45ae81ae2f0
RayanPIOCTMOUDNIP22011981 <rayan.pioct@etu.univ-amu.fr>
2023-10-02 10:59:53 +0200

    Sauvegarde html index

--- index.php
+++ index.php
@@ -2,10 +2,18 @@
 <html lang="fr">
 <head>
     <title> login </title>
-    <link rel="stylesheet" type="text/css" href="_Assets/style/style.css">
+    <link rel="stylesheet" type="text/css" href="_Assets/Styles/style.css">
     <link rel="icon" type="favicon.ico" href="_Assets/Images/favicon.ico">
 </head>
 <body>
+    <!-- Menu permanent -->
+    <ul id="menu">
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Catégories</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contacts</a></li>
+    </ul>
+    <!-- Formulaire du LOGIN -->
     <form action="login.php" method="post">
         <h2> LOGIN</h2>
         <?php if(isset($_GET['error'])) {?>

ea2a268c832809de6240f47ee06b8d2e81c58ad1
Valointain <125372238+ROSSI-Valentin-2225072a@users.noreply.github.com>
2023-11-05 20:02:38 +0100

    -Connexion à la base de donnée sur le site établie

--- _assets/includes/DatabaseConnection.php
+++ _assets/includes/DatabaseConnection.php
@@ -5,8 +5,8 @@ class DatabaseConnection
     public static function connect(): PDO
     {
 
-        $dsn = 'mysql:host=localhost;dbname=forum';
-        $pdo = new PDO($dsn, 'forumDB', 'pass');
+        $dsn = 'mysql:host=mysql-wang-m.alwaysdata.net;dbname=wang-m_forum';
+        $pdo = new PDO($dsn, 'wang-m_forumdb', 'passdbwang');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
 

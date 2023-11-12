b1b7ede1a1b9a36778f728715900da41430930e6
Lorys <114394383+Guigstyyle@users.noreply.github.com>
2023-11-12 18:30:56 +0100

    Update PostController.php

--- module/controller/PostController.php
+++ module/controller/PostController.php
@@ -143,7 +143,7 @@ class PostController
     {
         $ticket = new TicketModel($_POST['idticket']);
         try {
-            if (!$this->verifyAuthor($ticket) && $_SESSION['user']->getAdministrator() === 0) {
+            if ((!$this->verifyAuthor($ticket)) && ($_SESSION['user']->getAdministrator() === 0)) {
                 throw new Exception('Vous ne pouvez pas supprimer les billets des autres.');
             }
             TicketModel::deleteTicket($_POST['idticket']);
